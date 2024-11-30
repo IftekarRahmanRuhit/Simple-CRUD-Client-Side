6282bcfb612bc5b5cb3d232c6a55d1ba99260f69
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-11-30 14:55:51 +0600

    added delete user functionality

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,18 +1,27 @@
 
 /* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const users = () => {
 
-    const users = useLoaderData()
+    const loadedUsers = useLoaderData()
+    const [users ,setUsers] = useState(loadedUsers)
 
     const handleDelete = _id => {
         console.log( 'Delete' ,_id)
-        fetch(`http://localhost:5000/users/${_id}`)
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE'
+        })
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            if(data.deletedCount>0 ){
+                alert('User Deleted Successfully')
+                const remaining = users.filter(user => user._id !== _id)
+                setUsers(remaining)
+            }
         })
     }
 
