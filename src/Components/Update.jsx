aad3b0f28c2d2bfa5d11b8713bf98e1210899fbc
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-11-30 23:11:06 +0600

    added update functionality

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -3,11 +3,27 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
   const loadedUser = useLoaderData();
 
-  const handleSubmit = event =>{
+  const handleUpdate = event =>{
     event.preventDefault()
     const form = event.target;
     const name= form.name.value;
     const email = form.email.value;
+    const updateUser ={name,email}
+
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updateUser)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      if(data.modifiedCount > 0){
+        alert('User Updated successfully')
+      }
+    })
   }
 
 
@@ -16,7 +32,7 @@ const Update = () => {
     <div>
       <p>Update information of {loadedUser.name}</p>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleUpdate}>
         Name : <input type="text" name="name" defaultValue={loadedUser?.name} id="" />
         <br />
         Email: <input type="email" name="email" defaultValue={loadedUser?.email} id="" />
